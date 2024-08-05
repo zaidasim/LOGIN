032f29d7cbb01280e854f00eb0a435e159a26854
Muhammad <zaidasim94@gmail.com>
2024-08-05 15:59:34 +0500

    added branch

--- my_login/login_page/urls.py
+++ my_login/login_page/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/',views.login_view,name='display'),
     path('auth/',views.auth_view,name='auth'),
     path('create/',views.createUser , name='create'),
+    path('update/',views.update,name='update')
 ]
--- my_login/login_page/views.py
+++ my_login/login_page/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm 
+from django.shortcuts import render,redirect , get_object_or_404
 from .forms import CreateForm
 from .models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
@@ -28,3 +30,14 @@ def createUser(request):
     else:
         form = CreateForm()
     return render(request,'create.html',{'form':form})
+@login_required
+def update(request):
+    profile, created = User.objects.get_or_create(user=request.user.email)
+    if request.method == 'POST':
+        form = CreateForm(request.POST,instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('display')
+    else:
+        form=CreateForm(instance=profile)
+    return render(request, 'profile_update.html' , {'form':form})
